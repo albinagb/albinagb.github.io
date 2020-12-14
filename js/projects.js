04afe0e7e832ce08ed74e22928cf18228222ea91
Alba G <3.1415@gmail.com>
2020-12-13 20:35:57 -0500

    Updated CSS
    Updated CSS on the Home page. Still it is very raw

--- js/projects.js
+++ js/projects.js
@@ -5,7 +5,7 @@ let button = document.querySelector("#swap");
 let mainWidth = document.querySelector(".main").offsetWidth;
 let mainHeight = document.querySelector(".main").offsetHeight;
 
-console.log(mainWidth, mainHeight);
+// console.log(mainWidth, mainHeight);
 
 button.addEventListener("click", () => {
   let collision = true;
@@ -17,8 +17,8 @@ button.addEventListener("click", () => {
     let itemWidth;
     Array.from(currencyElems).forEach((el, i) => {
       let to = {
-        x: Math.random() * (i % 2 === 0 ? -15 : 15),
-        y: Math.random() * 11,
+        x: Math.random() * (i % 2 === 0 ? -11 : 11),
+        y: Math.random() * 5,
       };
 
       el.animate(
@@ -45,27 +45,19 @@ button.addEventListener("click", () => {
       pointsArr.push([x, y]);
     });
 
+    // Points Array => ForEach
+
     let a = pointsArr[0][0] - pointsArr[1][0];
     let b = pointsArr[0][1] - pointsArr[1][1];
     let distance = Math.sqrt(a * a + b * b);
 
     if (distance > itemWidth * 1.05) {
       collision = false;
-      console.log(distance, distance > itemWidth * 1.1, attempts);
+      // console.log(distance, distance > itemWidth * 1.1, attempts);
     }
   }
 });
 
 // Collision code
 
-function changeColor() {
-  let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  return randomColor;
-}
 
-function intersects(el) {
-  let rect = el.getBoundingClientRect();
-  console.log(
-    `T: ${rect.top}, R: ${rect.right}, B: ${rect.bottom}, L: ${rect.left}`
-  );
-}
