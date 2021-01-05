4d8ac3e0a3d06f8c7fa21249f0dd12aecd9d0478
Alba G <3.1415@gmail.com>
2021-01-05 13:54:46 -0500

    CSS Update

--- js/script.js
+++ js/script.js
@@ -15,9 +15,24 @@ let arr = [];
 const btn = document.querySelector(".btn-toggle");
 
 btn.addEventListener("click", function () {
-  console.log("hello Mir");
-  var element = document.body;
-  element.classList.toggle("summer-theme");
+  let element = document.body;
+  let sunElems = document.querySelectorAll(".sunLight");
+  let triangleElems = document.querySelectorAll(".triangleOvals");
+  let earthElems = document.querySelectorAll(".rectEarth");
+
+  element.classList.toggle("warmBckg");
+
+  sunElems.forEach((e) => {
+    e.classList.toggle("warmSun");
+  });
+
+  earthElems.forEach((e) => {
+    e.classList.toggle("rectangleBckg");
+  });
+
+  triangleElems.forEach((e) => {
+    e.classList.toggle("triangleBckg");
+  });
 });
 
 // Create selections
